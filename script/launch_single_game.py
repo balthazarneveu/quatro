e4b37cb3e1e464aef864db14dfb6fb5ae94fe08f
Balthazar Neveu <balthazarneveu@gmail.com>
2025-01-12 15:12:43 +0100

    ⚙️ #3 allow picking among several games

--- script/launch_single_game.py
+++ script/launch_single_game.py
@@ -1,4 +1,22 @@
-from quatro.flappy_butterfly import launch_flappy_pipe
+from quatro.games.flappy_butterfly_interactive_pipe import launch_flappy_pipe
+import argparse
+import sys
+
+GAMES_LIST = {"flappy_pipe": launch_flappy_pipe}
+GAMES_NAMES = list(GAMES_LIST.keys())
+
+
+def populate_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-g", "--game", type=str, default="flappy_pipe", choices=GAMES_NAMES)
+    return parser
+
 
 if __name__ == "__main__":
-    launch_flappy_pipe()
+    parser = populate_parser()
+    args = parser.parse_args(sys.argv[1:])
+    game_to_launch = args.game
+    assert game_to_launch is not None, f"Game {game_to_launch} not found in GAMES_LIST"
+    game_func = GAMES_LIST.get(game_to_launch, None)
+    assert game_func is not None, f"Game {game_to_launch} not found in GAMES_LIST"
+    game_func()
