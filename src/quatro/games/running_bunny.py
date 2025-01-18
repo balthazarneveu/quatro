c95f9b882705e14e71ba42ac5e492c5d52d76406
Balthazar Neveu <balthazarneveu@gmail.com>
2025-01-18 15:53:50 +0100

    ⚙️ #3 moving plank system

--- src/quatro/games/running_bunny.py
+++ src/quatro/games/running_bunny.py
@@ -2,15 +2,19 @@ import pygame
 from quatro.graphics.background import draw_background_from_asset
 from quatro.graphics.animation.bunny import Bunny
 from quatro.system.quit import handle_quit
+import random
 
 
 class Hole:
     def __init__(self, x, y, z=0):
         self.x = x
         self.y = y
+        self.width = 80
 
     def draw(self, screen):
-        pygame.draw.ellipse(screen, (0, 0, 0), (self.x, self.y, 80, 40))
+        pygame.draw.ellipse(
+            screen, (0, 0, 0), (self.x - self.width / 2, self.y, self.width, 40)
+        )
 
 
 class Camera:
@@ -57,38 +61,70 @@ def clip(value, min_value, max_value):
     return min(max(value, min_value), max_value)
 
 
-class Sandtrack:
+class SandTrack:
+    def __init__(self, num_planks=10, z_source=10.0):
+        self.z_source = z_source
+        self.planks = [
+            Plank(
+                z=z_source - (i / num_planks) * z_source,
+                intensity=0.5 + i / num_planks,
+                x=random.uniform(-1, 1.0),
+                plank_depth=1.0,
+            )
+            for i in range(num_planks)
+        ]
+
+    def move_planks(self, dt: float = 0.1):
+        global camera
+        for plank in self.planks:
+            plank.z -= dt
+            if plank.z <= 0.5:
+                plank.z = self.z_source
+                plank.x = random.uniform(-1, 1)
+
+    def draw(self, screen: pygame.Surface):
+        for plank in self.planks:
+            plank.draw(screen)
+
+
+class Plank:
     def __init__(
         self,
         x: float = 0,  # => in the middle horizontally
         y: float = 0,  # => on the floor
         z: float = 10.0,  # => 10 units away from the screen
+        plank_depth: float = 5.0,
+        plank_width: float = 3.0,
+        color=(139, 69, 19),
+        intensity=1.0,
     ):
         self.x = x
         self.y = y
         self.z = z
+        self.color = [_color * intensity for _color in color]
+        self.plank_depth = plank_depth
+        self.plank_width = plank_width
 
-    def draw_flat_plank(
+    def draw(
         self,
         screen: pygame.Surface,
     ):
         global camera
         # 3D plank coordinates
-        width = 3.0
-        depth = 5.0
-        top = clip(self.z + depth // 2, camera.min_distance, None)
-        bottom = clip(self.z - depth // 2, camera.min_distance, None)
-        tl = pygame.Vector3(self.x - width // 2, self.y, top)
-        tr = pygame.Vector3(self.x + width // 2, self.y, top)
-        br = pygame.Vector3(self.x + width // 2, self.y, bottom)
-        bl = pygame.Vector3(self.x - width // 2, self.y, bottom)
-
+        left = self.x - self.plank_width / 2
+        right = self.x + self.plank_width / 2
+        top = clip(self.z + self.plank_depth / 2, camera.min_distance, None)
+        bottom = clip(self.z - self.plank_depth / 2, camera.min_distance, None)
+        tl = pygame.Vector3(left, self.y, top)
+        tr = pygame.Vector3(right, self.y, top)
+        br = pygame.Vector3(right, self.y, bottom)
+        bl = pygame.Vector3(left, self.y, bottom)
         # 2D screen coordinates
         pts_3d = [tl, tr, br, bl]
         # Visibility check
         if all([pt_3d.z >= 0 for pt_3d in pts_3d]):
             points = [camera.project(pt_3d) for pt_3d in pts_3d]
-            pygame.draw.polygon(screen, (139, 69, 19), points)
+            pygame.draw.polygon(screen, self.color, points)
         pass
 
 
@@ -101,7 +137,8 @@ def launch_running_bunny():
     clock = pygame.time.Clock()
     running = True
     dt = 0
-    plank = Sandtrack()
+    plank = Plank()
+    sandtrack = SandTrack()
 
     player_pos = pygame.Vector2(w / 2, 3 * h / 4)
     player = Bunny(*player_pos, size=50, animation_speed=10)
@@ -110,23 +147,21 @@ def launch_running_bunny():
         keys = pygame.key.get_pressed()
         running = handle_quit(keys)
         draw_background_from_asset(screen, current_background)
-        plank.draw_flat_plank(screen)
+
+        sandtrack.move_planks(dt=dt)
+        sandtrack.draw(screen)
         player.draw(screen, dt=dt)
 
         # Draw black holes
         hole = Hole(600, 650)  # looks like  a shadow
+        hole.x = player.x
         hole.draw(screen)
         # Game control update logic
         if keys[pygame.K_LEFT]:
-            # player.x -= 100 * dt
-            plank.x -= 1 * dt
+            player.x -= 100 * dt
+
         if keys[pygame.K_RIGHT]:
-            plank.x += 1 * dt
-            # player.x += 100 * dt
-        if keys[pygame.K_UP]:
-            plank.z += 0.5 * dt
-        if keys[pygame.K_DOWN]:
-            plank.z -= 2 * dt
+            player.x += 100 * dt
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
