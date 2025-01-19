2d122b61af92a337f549ee9d807b0a4cdaa8abca
Balthazar Neveu <balthazarneveu@gmail.com>
2025-01-19 23:16:29 +0100

    ⚙️ #3 proper fullscreen support

--- src/quatro/system/window.py
+++ src/quatro/system/window.py
@@ -4,7 +4,9 @@ from typing import Tuple, Optional
 
 def init_screen(resolution: Optional[Tuple[int, int]] = None) -> pygame.Surface:
     pygame.init()
+    flags = pygame.NOFRAME
     if resolution is None:
         resolution = (pygame.display.Info().current_w, pygame.display.Info().current_h)
-    screen = pygame.display.set_mode(resolution, pygame.NOFRAME)
+        flags |= pygame.FULLSCREEN
+    screen = pygame.display.set_mode(resolution, flags)
     return screen
